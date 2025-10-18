ea057d8147b512d7d04543f31c52e4a4b921d5a4
rhodes-b <59537185+rhodes-b@users.noreply.github.com>
2025-10-18 00:49:28 -0500

    remove usage of cImport

--- build.zig
+++ build.zig
@@ -4,14 +4,50 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const rsgl_gl = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .root_source_file = null,
+    });
+
+    const rsgl_gl1 = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .root_source_file = null,
+    });
+
+    rsgl_gl.addCSourceFiles(.{
+        .files = &.{
+            "RSGL.h",
+            "renderers/RSGL_gl.h",
+        },
+        .flags = &.{"-DRSGL_IMPLEMENTATION"},
+        .language = .c,
+    });
+
+    rsgl_gl1.addCSourceFiles(.{
+        .files = &.{
+            "RSGL.h",
+            "renderers/RSGL_gl1.h",
+        },
+        .flags = &.{"-DRSGL_IMPLEMENTATION"},
+        .language = .c,
+    });
+
     const mod = b.addModule("RSGL", .{
         .target = target,
         .optimize = optimize,
         .link_libc = true,
         .root_source_file = b.path("root.zig")
     });
-    mod.addIncludePath(b.path("."));
 
+    mod.addImport("rsgl_gl", rsgl_gl);
+    mod.addImport("rsgl_gl1", rsgl_gl1);
+
+    mod.addIncludePath(b.path("."));
+    
     switch (target.result.os.tag) {
         .linux, .freebsd, .openbsd, .dragonfly => {
             mod.linkSystemLibrary("GL", .{.needed = true});
--- root.zig
+++ root.zig
@@ -1,11 +1,4 @@
-pub const RSGL_gl = @cImport({
-    @cDefine("RSGL_IMPLEMENTATION", "");
-    @cInclude("RSGL.h");
-    @cInclude("renderers/RSGL_gl.h");
-});
+pub const RSGL_gl = @import("rsgl_gl");
 
-pub const RSGL_gl1 = @cImport({
-    @cDefine("RSGL_IMPLEMENTATION", "");
-    @cInclude("RSGL.h");
-    @cInclude("renderers/RSGL_gl1.h");
-});
+pub const RSGL_gl1 = @import("rsgl_gl1");
+
